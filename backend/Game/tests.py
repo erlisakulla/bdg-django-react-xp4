ccf5c21d3702b1f4b6b4e269410df596e09d8eaf
Ishwor Giri <ishworgiri1999@gmail.com>
2021-03-23 23:57:45 +0100

    more test

--- backend/Game/tests.py
+++ backend/Game/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from .models import game
 from User.models import User
+
+from rest_framework.test import APITestCase
 # Create your tests here.
 class gameTest(TestCase):
     """ Test module for Game model """
@@ -14,3 +16,25 @@ class gameTest(TestCase):
         gameok = game.objects.get(instructor=userok)
         self.assertEqual(
             gameok.session_length, 55)
+
+
+
+class UserRegistrationAPIViewTestCase(APITestCase):
+    url = '/game/'
+
+    def test_without_authorization(self):
+        """
+Without authorization
+        """
+
+        cuser= User.objects.create(
+email='yahoo@gmail.com', name="ok", password="whatever111",is_instructor=True)
+        data = {
+            "session_length":1 ,
+            "is_wholesalor_present": True,
+            "in_distributor_present": True,
+            "instructor":cuser
+        }
+        response = self.client.post(self.url, data)
+        self.assertEqual(401, response.status_code)
+
